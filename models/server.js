8cf5f19d0a9d2d51763212715eae962d94a6a804
Ítalo Ramírez P <italo.ramirez@activeone.co>
2023-04-11 21:07:29 -0500

    crea middleware para carpeta pública

--- models/server.js
+++ models/server.js
@@ -4,11 +4,18 @@ class Server {
     constructor () {
         this.app = express()
         this.port = process.env.PORT
+        //Middlewares => funciones que van a añadir otra funcionalidad
+        this.middlewares()
+        //routes
         this.routes()
     }
 
-    routes () {
+    middlewares () {
+        //Directorio público
+        this.app.use(express.static('public'))
+    }
 
+    routes () {
         this.app.get('/', (req, res) => {
             res.send('hola mundo')
         })
